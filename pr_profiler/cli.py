1a7cb72c197d445a1fb759b0ac59340340ef26ac
Artur Fonseca <artur.fonseca.souza@gmail.com>
2025-11-21 13:56:31 -0300

    feat(cli): adiciona spinner de carregamento e tratamento de erros no comando analyze

--- pr_profiler/cli.py
+++ pr_profiler/cli.py
@@ -1,8 +1,11 @@
 import typer
+import sys
+from rich.console import Console
 from pr_profiler.analysis import run_analysis
 from pr_profiler.presentation import display_report
 
 app = typer.Typer(no_args_is_help=True)
+console = Console()
 
 @app.callback()
 def main():
@@ -14,11 +17,22 @@ def analyze(repo: str):
     """
     Analisa um repositório em busca de problemas de processo.
     """
-    # Chama a lógica (Analysis)
-    report = run_analysis(repo)
-    
-    # Chama a visualização (Presentation)
-    display_report(report)
+    console.print(f"[bold blue]Iniciando PR Profiler para:[/bold blue] {repo}")
+
+    try:
+        # O 'with console.status' cria a animação de carregamento
+        with console.status("[bold green]Minerando dados do GitHub API... (Isso pode levar alguns segundos)[/bold green]", spinner="dots"):
+            report = run_analysis(repo)
+        
+        display_report(report)
+
+    except ValueError as e:
+        console.print(f"\n[bold red]❌ Erro:[/bold red] {str(e)}")
+        sys.exit(1)
+        
+    except Exception as e:
+        console.print(f"\n[bold red]💥 Erro Inesperado:[/bold red] {str(e)}")
+        sys.exit(1)
 
 if __name__ == "_main_":
     app()
